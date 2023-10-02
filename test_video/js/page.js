de287e4684043b35b15e356d6fbb5ce29913797b
Snow <daniteddy@gmail.com>
2023-10-02 17:24:08 +0800

    add

---
+++ test_video/js/page.js
@@ -0,0 +1,7 @@
+$(window).on('load resize', function(){
+  // container height
+  var headH = $('header').outerHeight();
+  var footH = $('footer').outerHeight();
+  var containerMin = $(window).height() - headH - footH;
+  $('.container').css('min-height', containerMin);
+});
