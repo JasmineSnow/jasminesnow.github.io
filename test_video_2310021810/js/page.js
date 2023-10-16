f8fd62fe438918b75397249a5c4c5c4ca52b3413
Snow <daniteddy@gmail.com>
2023-10-16 18:02:54 +0800

    dele all

--- test_video_2310021810/js/page.js
+++
@@ -1,7 +0,0 @@
-$(window).on('load resize', function(){
-  // container height
-  var headH = $('header').outerHeight();
-  var footH = $('footer').outerHeight();
-  var containerMin = $(window).height() - headH - footH;
-  $('.container').css('min-height', containerMin);
-});
